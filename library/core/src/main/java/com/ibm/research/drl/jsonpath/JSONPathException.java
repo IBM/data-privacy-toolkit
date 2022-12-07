816925b6baf596b56e9259895817f5a0beba92e8
Stefano Braghin <stefano.braghin@gmail.com>
2022-12-07 13:17:43 +0000

    Porting json path library first
    Signed-off-by: Stefano Braghin <stefano.braghin@gmail.com>

---
+++ library/core/src/main/java/com/ibm/research/drl/jsonpath/JSONPathException.java
@@ -0,0 +1,16 @@
+/*******************************************************************
+ *                                                                 *
+ * Copyright IBM Corp. 2016                                        *
+ *                                                                 *
+ *******************************************************************/
+package com.ibm.research.drl.jsonpath;
+
+public class JSONPathException extends Exception {
+    public JSONPathException() {
+        super();
+    }
+
+    public JSONPathException(String message) {
+        super(message);
+    }
+}
