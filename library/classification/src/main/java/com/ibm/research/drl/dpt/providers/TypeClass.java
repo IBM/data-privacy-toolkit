556a3a1841faf6afb97de01ac17752d70ada9a37
Stefano Braghin <527806+stefano81@users.noreply.github.com>
2022-12-08 18:39:17 +0000

    fix swiftCode manager test

---
+++ library/classification/src/main/java/com/ibm/research/drl/dpt/providers/TypeClass.java
@@ -0,0 +1,17 @@
+/*******************************************************************
+ *                                                                 *
+ * Copyright IBM Corp. 2021                                        *
+ *                                                                 *
+ *******************************************************************/
+package com.ibm.research.drl.dpt.providers;
+
+public enum TypeClass {
+    /**
+     * Numerical type class.
+     */
+    NUMERICAL,
+    /**
+     * Categorical type class.
+     */
+    CATEGORICAL
+}
