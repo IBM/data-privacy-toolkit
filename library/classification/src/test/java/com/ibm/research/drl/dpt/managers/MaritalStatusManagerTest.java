42283a6a4500443763475dee94125d7cb9076726
Stefano Braghin <527806+stefano81@users.noreply.github.com>
2022-12-08 18:49:46 +0000

    fix package name
    Signed-off-by: Stefano Braghin <527806+stefano81@users.noreply.github.com>

--- library/classification/src/test/java/com/ibm/research/drl/dpt/managers/MaritalStatusManagerTest.java
+++ library/classification/src/test/java/com/ibm/research/drl/dpt/managers/MaritalStatusManagerTest.java
@@ -1,12 +1,8 @@
 /*******************************************************************
-* IBM Confidential                                                *
-*                                                                 *
-* Copyright IBM Corp. 2021                                        *
-*                                                                 *
-* The source code for this program is not published or otherwise  *
-* divested of its trade secrets, irrespective of what has         *
-* been deposited with the U.S. Copyright Office.                  *
-*******************************************************************/
+ *                                                                 *
+ * Copyright IBM Corp. 2021                                        *
+ *                                                                 *
+ *******************************************************************/
 package com.ibm.research.drl.dpt.managers;
 
 import org.junit.jupiter.api.Test;
