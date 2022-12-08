6dd10ca886b19b3617089b6c1a12503e7b0d6a03
Stefano Braghin <stefano.braghin@gmail.com>
2022-12-08 18:53:03 +0000

    fix dependent manager test
    Signed-off-by: Stefano Braghin <stefano.braghin@gmail.com>

--- library/classification/src/test/java/com/ibm/research/drl/dpt/managers/DependentManagerTest.java
+++ library/classification/src/test/java/com/ibm/research/drl/dpt/managers/DependentManagerTest.java
@@ -1,13 +1,9 @@
-package com.ibm.research.drl.dpt.managers;
 /*******************************************************************
- * IBM Confidential                                                *
  *                                                                 *
  * Copyright IBM Corp. 2021                                        *
  *                                                                 *
- * The source code for this program is not published or otherwise  *
- * divested of its trade secrets, irrespective of what has         *
- * been deposited with the U.S. Copyright Office.                  *
  *******************************************************************/
+package com.ibm.research.drl.dpt.managers;
 
 import org.junit.jupiter.api.Test;
 
