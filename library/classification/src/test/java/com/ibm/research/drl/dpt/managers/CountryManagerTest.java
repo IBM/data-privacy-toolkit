a102f7bbe337938ad83e55d8f3656c619b1edae1
Stefano Braghin <527806+stefano81@users.noreply.github.com>
2022-12-08 18:47:17 +0000

    fix continent manager test
    Signed-off-by: Stefano Braghin <527806+stefano81@users.noreply.github.com>

--- library/classification/src/test/java/com/ibm/research/drl/dpt/managers/CountryManagerTest.java
+++ library/classification/src/test/java/com/ibm/research/drl/dpt/managers/CountryManagerTest.java
@@ -1,15 +1,11 @@
 /*******************************************************************
-* IBM Confidential                                                *
-*                                                                 *
-* Copyright IBM Corp. 2021                                        *
-*                                                                 *
-* The source code for this program is not published or otherwise  *
-* divested of its trade secrets, irrespective of what has         *
-* been deposited with the U.S. Copyright Office.                  *
-*******************************************************************/
+ *                                                                 *
+ * Copyright IBM Corp. 2021                                        *
+ *                                                                 *
+ *******************************************************************/
 package com.ibm.research.drl.dpt.managers;
 
-import com.ibm.research.drl.prima.util.CountryNameSpecification;
+import com.ibm.research.drl.dpt.util.CountryNameSpecification;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
