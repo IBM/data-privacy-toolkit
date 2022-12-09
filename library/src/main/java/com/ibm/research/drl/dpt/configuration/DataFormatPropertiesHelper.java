1d3b0e7b99f9ce3177a7726ec87a889de6eae5ee
Stefano Braghin <527806+stefano81@users.noreply.github.com>
2022-12-09 10:35:51 +0000

    Fix DataMaskingConfiguration
    Signed-off-by: Stefano Braghin <527806+stefano81@users.noreply.github.com>

--- library/src/main/java/com/ibm/research/drl/dpt/configuration/DataFormatPropertiesHelper.java
+++ library/src/main/java/com/ibm/research/drl/dpt/configuration/DataFormatPropertiesHelper.java
@@ -1,27 +1,25 @@
 /*******************************************************************
  *                                                                 *
- * Copyright IBM Corp. 2022                                        *
+ * Copyright IBM Corp. 2021                                        *
  *                                                                 *
  *******************************************************************/
 package com.ibm.research.drl.dpt.configuration;
 
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.ibm.research.drl.dpt.util.JsonUtils;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DataFormatPropertiesHelper {
+    private final static ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     public static Map<DataTypeFormat, DataFormatProperties> buildProperties(InputStream inputStream) throws IOException {
         Map<DataTypeFormat, DataFormatProperties> results = new HashMap<>();
 
-        JsonNode contents = JsonUtils.MAPPER.readTree(inputStream);
+        JsonNode contents = OBJECT_MAPPER.readTree(inputStream);
 
         contents.fields().forEachRemaining(entry -> {
             String key = entry.getKey();
@@ -35,12 +33,12 @@ public class DataFormatPropertiesHelper {
             });
 
             DataFormatProperties properties = new DataFormatProperties(
-                    jsonNode.get("identification").asBoolean(),
-                    jsonNode.get("vulnerabilityAssessment").asBoolean(),
-                    jsonNode.get("masking").asBoolean(),
-                    jsonNode.get("anonymization").asBoolean(),
-                    jsonNode.get("freeText").asBoolean(),
-                    validOutputs
+               jsonNode.get("identification").asBoolean(),
+               jsonNode.get("vulnerabilityAssessment").asBoolean(),
+               jsonNode.get("masking").asBoolean(),
+               jsonNode.get("anonymization").asBoolean(),
+               jsonNode.get("freeText").asBoolean(),
+               validOutputs
             );
 
             results.put(DataTypeFormat.valueOf(key), properties);
