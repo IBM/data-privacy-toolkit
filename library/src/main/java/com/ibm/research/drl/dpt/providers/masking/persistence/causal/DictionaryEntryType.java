d621b769bf1c5411d54a51766633402c7116580a
Stefano Braghin <stefano.braghin@gmail.com>
2022-12-09 14:55:59 +0000

    add more masking providers and corresponding tests
    Signed-off-by: Stefano Braghin <stefano.braghin@gmail.com>

---
+++ library/src/main/java/com/ibm/research/drl/dpt/providers/masking/persistence/causal/DictionaryEntryType.java
@@ -0,0 +1,13 @@
+/*******************************************************************
+ *                                                                 *
+ * Copyright IBM Corp. 2021                                        *
+ *                                                                 *
+ *******************************************************************/
+package com.ibm.research.drl.dpt.providers.masking.persistence.causal;
+
+public enum DictionaryEntryType {
+    INSERT,
+    DELETE,
+    VALUE
+}
+
