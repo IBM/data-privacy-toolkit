e8cdbbe2cc941ddcca9a62a5e2ac684751f7f790
Stefano Braghin <stefano.braghin@gmail.com>
2022-12-09 00:13:28 +0000

    porting configuration file readers and models
    Signed-off-by: Stefano Braghin <stefano.braghin@gmail.com>

---
+++ library/src/main/java/com/ibm/research/drl/dpt/schema/RelationshipType.java
@@ -0,0 +1,43 @@
+/*******************************************************************
+ *                                                                 *
+ * Copyright IBM Corp. 2021                                        *
+ *                                                                 *
+ *******************************************************************/
+package com.ibm.research.drl.dpt.schema;
+
+public enum RelationshipType {
+    /**
+     * Sum relationship type.
+     */
+    SUM,
+    /**
+     * Sum approximate relationship type.
+     */
+    SUM_APPROXIMATE,
+    /**
+     * Product relationship type.
+     */
+    PRODUCT,
+    /**
+     * Equals relationship type.
+     */
+    EQUALS,
+    /**
+     * Greater relationship type.
+     */
+    GREATER,
+    /* For dates */
+    DISTANCE,
+    /**
+     * Less relationship type.
+     */
+    LESS,
+    /**
+     * Linked relationship type.
+     */
+    LINKED,
+
+    KEY,
+
+    GREP_AND_MASK /* for freetext */
+}
