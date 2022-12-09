08e4ed464f13accc76ecf65425e147a35810bae2
Stefano Braghin <stefano.braghin@gmail.com>
2022-12-09 10:53:01 +0000

    Porting more tests
    Signed-off-by: Stefano Braghin <stefano.braghin@gmail.com>

---
+++ library/src/test/java/com/ibm/research/drl/dpt/util/localization/LocalizationManagerTest.java
@@ -0,0 +1,102 @@
+/*******************************************************************
+ *                                                                 *
+ * Copyright IBM Corp. 2022                                        *
+ *                                                                 *
+ *******************************************************************/
+package com.ibm.research.drl.dpt.util.localization;
+
+import com.ibm.research.drl.dpt.providers.identifiers.RaceEthnicityIdentifier;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Objects;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LocalizationManagerTest {
+    private final LocalizationManager manager = LocalizationManager.getInstance();
+
+    @Test
+    public void initialization() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void getAllResources() {
+        for (Resource resource : Resource.values()) {
+            Collection<ResourceEntry> resources = manager.getResources(resource);
+
+            assertNotNull(resources);
+            assertThat(resources.size(), is(not(0)));
+        }
+    }
+
+    @Test
+    public void getResourcesUsIsEn() {
+        Collection<ResourceEntry> resources = manager.getResources(Resource.COUNTRY, Collections.singleton("us"));
+
+        assertNotNull(resources);
+        assertThat(resources.size(), is(1));
+
+        for (ResourceEntry resourceEntry : resources)
+            assertThat(resourceEntry.getCountryCode(), is("en"));
+    }
+
+    @Test
+    public void getResourcesUkIsEn() {
+        Collection<ResourceEntry> resources = manager.getResources(Resource.COUNTRY, Collections.singleton("uk"));
+
+        assertNotNull(resources);
+        assertThat(resources.size(), is(1));
+
+        for (ResourceEntry resourceEntry : resources)
+            assertThat(resourceEntry.getCountryCode(), is("en"));
+    }
+
+    @Test
+    public void getResourcesWithCountry() {
+        for (Resource resource : Resource.values()) {
+            Collection<ResourceEntry> resources = manager.getResources(resource, Collections.singleton("us"));
+
+            assertNotNull(resources);
+        }
+    }
+
+    @Test
+    public void userCanRegisterANewCountry() {
+        manager.registerCountryCode("test");
+    }
+
+    @Test
+    public void userCannotRegisterAResourceForUnknownCountryCode() {
+        assertThrows(IllegalArgumentException.class, () -> manager.registerResource(Resource.CITY, "test123", "something something"));
+    }
+
+    @Test
+    public void userCanRegisterNewExternalResources() {
+        String filePath = Objects.requireNonNull(getClass().getResource("/identifier/races_fantasy.csv")).getPath();
+        String newCountryCode = "fantasy";
+
+        manager.registerCountryCode(newCountryCode);
+
+        manager.registerResource(Resource.RACE_ETHNICITY, newCountryCode, filePath);
+
+        Collection<ResourceEntry> resources = LocalizationManager.getInstance().getResources(Resource.RACE_ETHNICITY, Collections.singleton("fantasy"));
+
+        assertNotNull(resources);
+        assertThat(resources.size(), is(1));
+
+        ResourceEntry entry = resources.iterator().next();
+        assertEquals(entry.getCountryCode(), newCountryCode);
+
+        RaceEthnicityIdentifier identifier = new RaceEthnicityIdentifier();
+
+        assertNotNull(identifier);
+
+        assertTrue(identifier.isOfThisType("Goblin"));
+    }
+}
