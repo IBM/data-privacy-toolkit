b4c6aaa87a18f3645a5205d739780ec11a0aba9c
Stefano Braghin <stefano.braghin@gmail.com>
2022-12-09 10:43:41 +0000

    Porting more classes
    Signed-off-by: Stefano Braghin <stefano.braghin@gmail.com>

---
+++ library/src/main/java/com/ibm/research/drl/dpt/configuration/FailMode.java
@@ -0,0 +1,14 @@
+/*******************************************************************
+ *                                                                 *
+ * Copyright IBM Corp. 2021                                        *
+ *                                                                 *
+ *******************************************************************/
+package com.ibm.research.drl.dpt.configuration;
+
+
+public class FailMode {
+    public static final int RETURN_ORIGINAL = 1;
+    public static final int RETURN_EMPTY = 2;
+    public static final int THROW_ERROR = 3;
+    public static final int GENERATE_RANDOM = 4;
+}
