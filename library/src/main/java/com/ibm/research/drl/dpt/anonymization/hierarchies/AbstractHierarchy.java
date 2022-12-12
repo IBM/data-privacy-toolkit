db3c6d1de0ff14ed034ae6bb6c93862bc99900d6
Stefano Braghin <stefano.braghin@gmail.com>
2022-12-12 13:20:19 +0000

    consolidate hierarchy location
    Signed-off-by: Stefano Braghin <stefano.braghin@gmail.com>

---
+++ library/src/main/java/com/ibm/research/drl/dpt/anonymization/hierarchies/AbstractHierarchy.java
@@ -0,0 +1,13 @@
+/*******************************************************************
+ *                                                                 *
+ * Copyright IBM Corp. 2017                                        *
+ *                                                                 *
+ *******************************************************************/
+package com.ibm.research.drl.dpt.anonymization.hierarchies;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+
+public abstract class AbstractHierarchy implements GeneralizationHierarchy {
+    public AbstractHierarchy(JsonNode node) {}
+}
