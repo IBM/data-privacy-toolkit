ce7fa379c255ae520f2e3a783cf50d11d48e4c4b
Stefano Braghin <stefano.braghin@gmail.com>
2022-12-12 13:56:32 +0000

    fix missing files for proper testing
    Signed-off-by: Stefano Braghin <stefano.braghin@gmail.com>

---
+++ library/src/test/java/com/ibm/research/drl/dpt/anonymization/mondrian/MondrianPartitionTest.java
@@ -0,0 +1,18 @@
+/*******************************************************************
+ *                                                                 *
+ * Copyright IBM Corp. 2017                                        *
+ *                                                                 *
+ *******************************************************************/
+package com.ibm.research.drl.dpt.anonymization.mondrian;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+@Disabled("Missing proper testing")
+public class MondrianPartitionTest {
+    @Test
+    public void testNormalizedWidth() {
+          
+    }
+}
+
