276e381263948f11944765c00ea6aa76e830700d
Stefano Braghin <stefano.braghin@gmail.com>
2022-12-12 12:06:47 +0000

    porting OLA and anonymity checkers
    Signed-off-by: Stefano Braghin <stefano.braghin@gmail.com>

---
+++ library/src/main/java/com/ibm/research/drl/dpt/anonymization/ola/LatticeNode.java
@@ -0,0 +1,175 @@
+/*******************************************************************
+ *                                                                 *
+ * Copyright IBM Corp. 2015                                        *
+ *                                                                 *
+ *******************************************************************/
+package com.ibm.research.drl.dpt.anonymization.ola;
+
+import java.util.Collection;
+
+public class LatticeNode {
+    private final int[] values;
+    private double suppressionRate;
+    private Boolean isAnonymous = null;
+    private Double informationLoss = null;
+    private boolean tagged = false;
+
+    public boolean isTagged() {
+        return tagged;
+    }
+
+    public void setTagged(boolean tagged) {
+        this.tagged = tagged;
+    }
+
+    public Double getInformationLoss() {
+        return informationLoss;
+    }
+
+    public void setInformationLoss(Double informationLoss) {
+        this.informationLoss = informationLoss;
+    }
+
+    public Boolean getAnonymous() {
+        return isAnonymous;
+    }
+
+    public void setAnonymous(Boolean anonymous) {
+        isAnonymous = anonymous;
+    }
+
+    /**
+     * Gets suppression rate.
+     *
+     * @return the suppression rate
+     */
+    public double getSuppressionRate() {
+        return suppressionRate;
+    }
+
+    /**
+     * Sets suppression rate.
+     *
+     * @param suppressionRate the suppression rate
+     */
+    public void setSuppressionRate(double suppressionRate) {
+        this.suppressionRate = suppressionRate;
+    }
+
+    /**
+     * Get values int [ ].
+     *
+     * @return the int [ ]
+     */
+    public int[] getValues() {
+        return values;
+    }
+
+
+    /**
+     * Sum int.
+     *
+     * @return the int
+     */
+    public int sum() {
+        int sum = 0;
+
+        for(int i = 0; i < values.length; i++) {
+            sum += values[i];
+        }
+
+        return sum;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return !(null == o || !(o instanceof LatticeNode)) && equals((LatticeNode) o);
+    }
+
+    /**
+     * Equals boolean.
+     *
+     * @param otherNode the other node
+     * @return the boolean
+     */
+    public boolean equals(LatticeNode otherNode) {
+        int[] otherValues = otherNode.getValues();
+
+        if (values.length != otherValues.length) {
+            return false;
+        }
+
+        for(int i = 0; i < values.length; i++) {
+            if (values[i] != otherValues[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Is descendent boolean.
+     *
+     * @param otherNode the other node
+     * @return the boolean
+     */
+    public boolean isDescendent(LatticeNode otherNode) {
+        if (this.equals(otherNode)) {
+            return false;
+        }
+
+        int[] otherValues = otherNode.getValues();
+
+        for(int i = 0; i < values.length; i++) {
+            if (values[i] < otherValues[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return toString().hashCode();
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder builder = new StringBuilder();
+        int i;
+        for(i = 0; i < (values.length - 1); i++) {
+            builder.append(values[i] + "");
+            builder.append(":");
+        }
+
+        builder.append(values[i] + "");
+        return builder.toString();
+    }
+
+    /**
+     * Instantiates a new Lattice node.
+     *
+     * @param values the values
+     */
+    public LatticeNode(int[] values) {
+        this.values = values;
+    }
+
+    /**
+     * Instantiates a new Lattice node.
+     *
+     * @param v the v
+     */
+    public LatticeNode(Collection<Integer> v) {
+        this.values = new int[v.size()];
+        int index = 0;
+        for(Integer i: v) {
+            this.values[index] = i.intValue();
+            index++;
+        }
+    }
+}
+
---
+++ library/src/test/java/com/ibm/research/drl/dpt/anonymization/ola/SimpleAnonymityCheckerTest.java
@@ -0,0 +1,111 @@
+/*******************************************************************
+ *                                                                 *
+ * Copyright IBM Corp. 2015                                        *
+ *                                                                 *
+ *******************************************************************/
+package com.ibm.research.drl.dpt.anonymization.ola;
+
+import com.ibm.research.drl.dpt.anonymization.CategoricalInformation;
+import com.ibm.research.drl.dpt.anonymization.ColumnInformation;
+import com.ibm.research.drl.dpt.anonymization.ColumnType;
+import com.ibm.research.drl.dpt.anonymization.PrivacyConstraint;
+import com.ibm.research.drl.dpt.anonymization.constraints.KAnonymity;
+import com.ibm.research.drl.dpt.anonymization.hierarchies.MaterializedHierarchy;
+import com.ibm.research.drl.dpt.datasets.IPVDataset;
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SimpleAnonymityCheckerTest {
+
+    @Test
+    public void testSuppressionRateCalculation() throws Exception {
+        int k = 3;
+
+        MaterializedHierarchy dateHierarchy = new MaterializedHierarchy();
+        dateHierarchy.add("01/01/2008", "Jan_2008", "2008");
+        dateHierarchy.add("02/01/2008", "Jan_2008", "2008");
+        dateHierarchy.add("03/01/2008", "Jan_2008", "2008");
+
+        MaterializedHierarchy genderHierarchy = new MaterializedHierarchy();
+        genderHierarchy.add("M", "Person");
+        genderHierarchy.add("F", "Person");
+
+        MaterializedHierarchy ageHierarchy = new MaterializedHierarchy();
+        ageHierarchy.add("13", "10-14", "10-19", "0-49", "0-99");
+        ageHierarchy.add("18", "15-19", "10-19", "0-49", "0-99");
+        ageHierarchy.add("19", "15-19", "10-19", "0-49", "0-99");
+        ageHierarchy.add("21", "20-24", "20-29", "0-49", "0-99");
+        ageHierarchy.add("22", "20-24", "20-29", "0-49", "0-99");
+        ageHierarchy.add("23", "20-24", "20-29", "0-49", "0-99");
+
+        List<ColumnInformation> columnInformationList = new ArrayList<>();
+        columnInformationList.add(new CategoricalInformation(dateHierarchy, ColumnType.QUASI));
+        columnInformationList.add(new CategoricalInformation(genderHierarchy, ColumnType.QUASI));
+        columnInformationList.add(new CategoricalInformation(ageHierarchy, ColumnType.QUASI));
+
+
+        final IPVDataset original;
+        try (InputStream inputStream = getClass().getResourceAsStream("/testOLA.csv")) {
+            original = IPVDataset.load(inputStream, false, ',', '"', false);
+        }
+
+        List<PrivacyConstraint> privacyConstraints = new ArrayList<>();
+        privacyConstraints.add(new KAnonymity(k));
+
+        SimpleAnonymityChecker simpleAnonymityChecker = new SimpleAnonymityChecker(original, columnInformationList, privacyConstraints);
+        LatticeNode node = new LatticeNode(new int[]{0, 0, 0});
+
+        double suppressionRate = simpleAnonymityChecker.calculateSuppressionRate(node);
+        //total records : 10,  only 3 rows are 3-anonymous
+        assertEquals(70.0d, suppressionRate, 0.1d);
+
+        node = new LatticeNode(new int[]{0, 0, 1});
+        suppressionRate = simpleAnonymityChecker.calculateSuppressionRate(node);
+        /*
+        we generalize age by 1 level. the new dataset should look like:
+            01/01/2008,M,15-19 x
+            01/01/2008,M,15-19 x
+            01/01/2008,M,15-19 x
+            01/01/2008,M,10-14
+            01/01/2008,M,15-19 x
+            02/01/2008,F,15-19
+            02/01/2008,F,20-24 x
+            02/01/2008,F,20-24 x
+            02/01/2008,F,20-24 x
+            01/01/2008,M,20-24
+
+            total records: 10, 3-anonymous rows: 7
+        */
+        assertEquals(30.0d, suppressionRate, 0.1d);
+
+        node = new LatticeNode(new int[]{2, 1, 4});
+        suppressionRate = simpleAnonymityChecker.calculateSuppressionRate(node);
+        //we generalize everything, suppression rate is 0
+        assertEquals(0.0d, suppressionRate, 0.001d);
+
+        node = new LatticeNode(new int[]{1, 0, 0});
+        /* dataset looks like:
+            JAN_2008,M,18, x
+            JAN_2008,M,18, x
+            JAN_2008,M,18, x
+            JAN_2008,M,13,
+            JAN_2008,M,19,
+            JAN_2008,F,18,
+            JAN_2008,F,22,
+            JAN_2008,F,23,
+            JAN_2008,F,21,
+            JAN_2008,M,22,
+         */
+        suppressionRate = simpleAnonymityChecker.calculateSuppressionRate(node);
+        assertEquals(70.0d, suppressionRate, 0.1d);
+
+        node = new LatticeNode(new int[]{1, 1, 1});
+        suppressionRate = simpleAnonymityChecker.calculateSuppressionRate(node);
+    }
+}
+
