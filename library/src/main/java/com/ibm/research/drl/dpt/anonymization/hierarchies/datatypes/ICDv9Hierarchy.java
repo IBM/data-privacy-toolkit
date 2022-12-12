bd9e5e72862823651f7c1fab54e7cd6116b14ee4
Stefano Braghin <stefano.braghin@gmail.com>
2022-12-12 10:43:25 +0000

    initial porting of included datatype hierarchies
    Signed-off-by: Stefano Braghin <stefano.braghin@gmail.com>

---
+++ library/src/main/java/com/ibm/research/drl/dpt/anonymization/hierarchies/datatypes/ICDv9Hierarchy.java
@@ -0,0 +1,34 @@
+/*******************************************************************
+ *                                                                 *
+ * Copyright IBM Corp. 2022                                        *
+ *                                                                 *
+ *******************************************************************/
+package com.ibm.research.drl.dpt.anonymization.hierarchies.datatypes;
+
+import com.ibm.research.drl.dpt.anonymization.hierarchies.MaterializedHierarchy;
+import com.ibm.research.drl.dpt.managers.ICDv9Manager;
+import com.ibm.research.drl.dpt.models.ICD;
+
+import java.util.Collection;
+
+public class ICDv9Hierarchy extends MaterializedHierarchy {
+    private final static ICDv9Manager ICD_V9_MANAGER = ICDv9Manager.getInstance();
+
+    private static final ICDv9Hierarchy instance = new ICDv9Hierarchy();
+    public static ICDv9Hierarchy getInstance() {return instance;}
+
+    private ICDv9Hierarchy() {
+        super();
+
+        Collection<ICD> icdList = ICD_V9_MANAGER.getItemList();
+        for(ICD icd: icdList) {
+            String[] terms = new String[4];
+            terms[0] = icd.getCode();
+            terms[1] = icd.getCategoryCode();
+            terms[2] = icd.getChapterCode();
+            terms[3] = "*";
+            add(terms);
+        }
+    }
+}
+
---
+++ library/src/main/java/com/ibm/research/drl/dpt/anonymization/hierarchies/datatypes/ZIPCodeMaterializedHierarchy.java
@@ -0,0 +1,31 @@
+/*******************************************************************
+ *                                                                 *
+ * Copyright IBM Corp. 2022                                        *
+ *                                                                 *
+ *******************************************************************/
+package com.ibm.research.drl.dpt.anonymization.hierarchies.datatypes;
+
+import com.ibm.research.drl.dpt.anonymization.hierarchies.MaterializedHierarchy;
+
+public class ZIPCodeMaterializedHierarchy extends MaterializedHierarchy {
+    private static final ZIPCodeMaterializedHierarchy instance = new ZIPCodeMaterializedHierarchy();
+    public static ZIPCodeMaterializedHierarchy getInstance() {return instance;}
+
+    private ZIPCodeMaterializedHierarchy() {
+        super();
+
+        for(int i = 0; i < 100_000; i++) {
+            String[] terms = new String[6];
+            
+            String zipcode = String.format("%05d", i);
+            terms[0] = zipcode;
+            terms[1] = zipcode.substring(0, 4) + "*";
+            terms[2] = zipcode.substring(0, 3) + "**";
+            terms[3] = zipcode.substring(0, 2) + "***";
+            terms[4] = zipcode.substring(0, 1) + "****";
+            terms[5] = "*";
+            add(terms);
+        }
+    }
+}
+
