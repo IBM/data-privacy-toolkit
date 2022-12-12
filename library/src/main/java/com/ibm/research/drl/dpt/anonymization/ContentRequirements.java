ed601b73cdfdfc006631f6ac134bd43caca734bb
Stefano Braghin <stefano.braghin@gmail.com>
2022-12-12 10:54:57 +0000

    initial porting of included privacy constraints, associated metrics and tests
    Signed-off-by: Stefano Braghin <stefano.braghin@gmail.com>

---
+++ library/src/main/java/com/ibm/research/drl/dpt/anonymization/ContentRequirements.java
@@ -0,0 +1,14 @@
+/*******************************************************************
+ *                                                                 *
+ * Copyright IBM Corp. 2017                                        *
+ *                                                                 *
+ *******************************************************************/
+package com.ibm.research.drl.dpt.anonymization;
+
+public class ContentRequirements {
+    public final static int NONE = 0;
+    public final static int NORMAL = 1;
+    public final static int QUASI = 2;
+    public final static int SENSITIVE = 4;
+}
+
