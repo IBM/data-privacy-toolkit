5bb814c276022ee88de2df866d82fe6710b9270a
Stefano Braghin <stefano.braghin@gmail.com>
2022-12-13 16:42:51 +0000

    add initial testing for anonymization
    Signed-off-by: Stefano Braghin <stefano.braghin@gmail.com>

---
+++ docker/src/test/java/com/ibm/research/drl/dpt/toolkit/anonymization/AnonymizationTaskTest.java
@@ -0,0 +1,36 @@
+/*******************************************************************
+ *                                                                 *
+ * Copyright IBM Corp. 2020                                        *
+ *                                                                 *
+ *******************************************************************/
+package com.ibm.research.drl.dpt.toolkit.anonymization;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+public class AnonymizationTaskTest {
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @Test
+    public void testAnonymizationTaskHappyPath() throws Exception {
+        try (InputStream inputStream = getClass().getResourceAsStream("/configuration_test_anonymization.json")) {
+            AnonymizationTask task = mapper.readValue(inputStream, AnonymizationTask.class);
+
+            assertNotNull(task);
+
+            try (
+                    InputStream input = this.getClass().getResourceAsStream("/healthcare-dataset.txt");
+                    OutputStream output = new ByteArrayOutputStream();
+            ) {
+                task.processFile(input, output);
+            }
+        }
+    }
+}
