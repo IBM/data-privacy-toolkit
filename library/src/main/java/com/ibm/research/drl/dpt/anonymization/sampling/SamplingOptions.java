c4c1255989a53e521151c1e208772e3978200f9d
Stefano Braghin <stefano.braghin@gmail.com>
2022-12-13 18:09:08 +0000

    add sampling anonymization approach
    Signed-off-by: Stefano Braghin <stefano.braghin@gmail.com>

---
+++ library/src/main/java/com/ibm/research/drl/dpt/anonymization/sampling/SamplingOptions.java
@@ -0,0 +1,33 @@
+/*******************************************************************
+ *                                                                 *
+ * Copyright IBM Corp. 2018                                        *
+ *                                                                 *
+ *******************************************************************/
+package com.ibm.research.drl.dpt.anonymization.sampling;
+
+
+import com.ibm.research.drl.dpt.anonymization.AnonymizationAlgorithmOptions;
+
+public class SamplingOptions implements AnonymizationAlgorithmOptions{
+    
+    private final double percentage;
+
+    public double getPercentage() {
+        return percentage;
+    }
+
+    @Override
+    public int getIntValue(String optionName) {
+        return 0;
+    }
+
+    @Override
+    public String getStringValue(String optionName) {
+        return null;
+    }
+    
+    public SamplingOptions(double percentage) {
+        this.percentage = percentage;
+    }
+}
+
