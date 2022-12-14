5748e7c7047c4de19682ff91239e51441a3ee130
Stefano Braghin <527806+stefano81@users.noreply.github.com>
2022-12-14 09:45:42 +0000

    amend copyright notice
    Signed-off-by: Stefano Braghin <527806+stefano81@users.noreply.github.com>

--- library/src/test/java/com/ibm/research/drl/dpt/configuration/IdentificationConfigurationTest.java
+++ library/src/test/java/com/ibm/research/drl/dpt/configuration/IdentificationConfigurationTest.java
@@ -1,3 +1,8 @@
+/*******************************************************************
+ *                                                                 *
+ * Copyright IBM Corp. 2018                                        *
+ *                                                                 *
+ *******************************************************************/
 package com.ibm.research.drl.dpt.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -7,15 +12,6 @@ import java.io.InputStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-/*******************************************************************
- * IBM Confidential                                                *
- *                                                                 *
- * Copyright IBM Corp. 2018                                        *
- *                                                                 *
- * The source code for this program is not published or otherwise  *
- * divested of its trade secrets, irrespective of what has         *
- * been deposited with the U.S. Copyright Office.                  *
- *******************************************************************/
 
 public class IdentificationConfigurationTest {
     private static final ObjectMapper mapper = new ObjectMapper();
