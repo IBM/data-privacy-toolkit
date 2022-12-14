72e6a6c761ebaed1798e4884bad9d12888b503e7
Stefano Braghin <stefano.braghin@gmail.com>
2022-12-14 09:45:42 +0000

    amend copyright notice
    Signed-off-by: Stefano Braghin <stefano.braghin@gmail.com>

--- library/src/main/java/com/ibm/research/drl/schema/IPVSchema.java
+++ library/src/main/java/com/ibm/research/drl/schema/IPVSchema.java
@@ -1,11 +1,7 @@
 /*******************************************************************
- * IBM Confidential                                                *
  *                                                                 *
  * Copyright IBM Corp. 2015                                        *
  *                                                                 *
- * The source code for this program is not published or otherwise  *
- * divested of its trade secrets, irrespective of what has         *
- * been deposited with the U.S. Copyright Office.                  *
  *******************************************************************/
 package com.ibm.research.drl.schema;
 
@@ -14,7 +10,6 @@ import java.util.List;
 /**
  * The interface Ipv schema.
  *
- * @author stefanob
  */
 public interface IPVSchema {
     /**
