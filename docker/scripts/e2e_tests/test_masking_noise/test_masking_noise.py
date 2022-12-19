328ae3d01eea3a26edffe0b33bcde4dfae5e88ed
Stefano Braghin <527806+stefano81@users.noreply.github.com>
2022-12-19 12:40:12 +0000

    porting of end to end tests for docker cli
    Signed-off-by: Stefano Braghin <527806+stefano81@users.noreply.github.com>

---
+++ docker/scripts/e2e_tests/test_masking_noise/test_masking_noise.py
@@ -0,0 +1,20 @@
+"""
+/*******************************************************************
+ * IBM Confidential                                                *
+ *                                                                 *
+ * Copyright IBM Corp. 2020                                        *
+ *                                                                 *
+ * The source code for this program is not published or otherwise  *
+ * divested of its trade secrets, irrespective of what has         *
+ * been deposited with the U.S. Copyright Office.                  *
+ *******************************************************************/
+"""
+
+
+def test_validate(docker_run, compare_csv_total_rows_and_cols):
+    """
+    The test.
+    """
+    docker_run(__file__)
+
+    compare_csv_total_rows_and_cols(__file__)
