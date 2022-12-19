7c0d87f972e159608c22071be5472d584e806da7
Stefano Braghin <stefano.braghin@gmail.com>
2022-12-19 14:22:51 +0000

    fix copyright disclaimer
    Signed-off-by: Stefano Braghin <stefano.braghin@gmail.com>

--- docker/scripts/e2e_tests/test_masking_noise/test_masking_noise.py
+++ docker/scripts/e2e_tests/test_masking_noise/test_masking_noise.py
@@ -1,13 +1,5 @@
 """
-/*******************************************************************
- * IBM Confidential                                                *
- *                                                                 *
- * Copyright IBM Corp. 2020                                        *
- *                                                                 *
- * The source code for this program is not published or otherwise  *
- * divested of its trade secrets, irrespective of what has         *
- * been deposited with the U.S. Copyright Office.                  *
- *******************************************************************/
+Copyright IBM Corp. 2020
 """
 
 
