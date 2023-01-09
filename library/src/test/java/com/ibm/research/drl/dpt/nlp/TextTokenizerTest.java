1d21a0c3c8f164813e6901418363ccb7361aca41
Stefano Braghin <527806+stefano81@users.noreply.github.com>
2023-01-09 12:46:21 +0000

    improve code coverage
    Signed-off-by: Stefano Braghin <527806+stefano81@users.noreply.github.com>

--- library/src/test/java/com/ibm/research/drl/dpt/nlp/TextTokenizerTest.java
+++ library/src/test/java/com/ibm/research/drl/dpt/nlp/TextTokenizerTest.java
@@ -14,23 +14,17 @@ import java.util.Iterator;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.lessThan;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TextTokenizerTest {
     private final static String text1 = "Tony Blair obtained a “blessing” from Chinese leaders for a company owned by a Saudi prince to do business in China as part of an arrangement that paid the former UK prime minister’s firm £41,000 a month and a 2% commission on any multimillion-pound contracts he helped to secure.";
     private final static String text2 = "CCTV footage released on Thursday showed a vicious assault in which Owen briefly touches shoulders with a Thai man carrying a bottle who appears to push him to the ground. An argument ensues and the family are set upon, repeatedly punched and kicked in the face even as they lie on the ground.";
-    private TextTokenizer tt;
-
-    @BeforeEach
-    public void setUp() throws Exception {
-        tt = new TextTokenizer();
-
-        assertNotNull(tt);
-    }
 
     @Test
     public void testEmails() throws IOException {
+        TextTokenizer tt = new TextTokenizer();
         String text = "Hello, my email is foo@gmail.com and i am going to the shops!!";
 
         List<String> words = new ArrayList<>();
@@ -44,6 +38,7 @@ public class TextTokenizerTest {
 
     @Test
     public void splitReturnSomething() throws IOException {
+        TextTokenizer tt = new TextTokenizer();
         List<String> words = new ArrayList<>();
 
         for (String word : tt.split(text1)) {
@@ -56,6 +51,7 @@ public class TextTokenizerTest {
 
     @Test
     public void splitReturnSomething2() throws IOException {
+        TextTokenizer tt = new TextTokenizer();
         List<String> words = new ArrayList<>();
 
         for (String word : tt.split(text2)) {
@@ -68,8 +64,40 @@ public class TextTokenizerTest {
 
     @Test
     public void splitHandlesEmpty() throws IOException {
+        TextTokenizer tt = new TextTokenizer();
         Iterator<String> iterator = tt.split("").iterator();
 
         assertFalse(iterator.hasNext());
+    }
+
+    @Test
+    public void testWithNoFilter() throws IOException {
+        TextTokenizer noFilter = new TextTokenizer(false);
+        assertNotNull(noFilter);
+
+        Iterator<String> noFilterIterator = noFilter.split(text2).iterator();
+
+        assertNotNull(noFilterIterator);
+
+        List<String> noFilteredToken = new ArrayList<>();
+
+        noFilterIterator.forEachRemaining(noFilteredToken::add);
+
+        assertFalse(noFilteredToken.isEmpty());
+
+        TextTokenizer withFilter = new TextTokenizer(true);
+        assertNotNull(noFilter);
+        Iterator<String> withFilterIterator = withFilter.split(text2).iterator();
+
+        assertNotNull(withFilterIterator);
+
+        List<String> withFilteredToken = new ArrayList<>();
+
+        withFilterIterator.forEachRemaining(withFilteredToken::add);
+
+        assertFalse(withFilteredToken.isEmpty());
+
+
+        assertThat(noFilteredToken.size(), greaterThan(withFilteredToken.size()));
     }
 }
