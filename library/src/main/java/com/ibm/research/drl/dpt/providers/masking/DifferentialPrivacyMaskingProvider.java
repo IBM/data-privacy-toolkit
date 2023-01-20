a716e13c0d3667e867cb572946c7d01057a838e9
Stefano Braghin <527806+stefano81@users.noreply.github.com>
2023-01-20 16:36:46 +0000

    add differential privacy as masking provider
    Signed-off-by: Stefano Braghin <527806+stefano81@users.noreply.github.com>

---
+++ library/src/main/java/com/ibm/research/drl/dpt/providers/masking/DifferentialPrivacyMaskingProvider.java
@@ -0,0 +1,75 @@
+/*******************************************************************
+ *                                                                 *
+ * Copyright IBM Corp. 2017                                        *
+ *                                                                 *
+ *******************************************************************/
+package com.ibm.research.drl.dpt.providers.masking;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.ibm.research.drl.dpt.anonymization.differentialprivacy.DPMechanism;
+import com.ibm.research.drl.dpt.anonymization.differentialprivacy.DPMechanismFactory;
+import com.ibm.research.drl.dpt.anonymization.differentialprivacy.DifferentialPrivacyMechanismOptions;
+import com.ibm.research.drl.dpt.anonymization.differentialprivacy.Mechanism;
+import com.ibm.research.drl.dpt.anonymization.hierarchies.GeneralizationHierarchy;
+import com.ibm.research.drl.dpt.configuration.AnonymizationOptions;
+import com.ibm.research.drl.dpt.configuration.DefaultMaskingConfiguration;
+import com.ibm.research.drl.dpt.configuration.MaskingConfiguration;
+import com.ibm.research.drl.dpt.exceptions.MisconfigurationException;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;;
+
+import java.security.SecureRandom;
+import java.util.Map;
+
+
+public class DifferentialPrivacyMaskingProvider extends AbstractMaskingProvider  {
+    private final static Logger logger = LogManager.getLogger(DifferentialPrivacyMaskingProvider.class);
+
+    private final DPMechanism mechanism;
+
+    public DifferentialPrivacyMaskingProvider(SecureRandom random, MaskingConfiguration configuration) {
+
+        DifferentialPrivacyMechanismOptions options = new DifferentialPrivacyMechanismOptions();
+        options.setEpsilon(configuration.getDoubleValue("differentialPrivacy.parameter.epsilon"));
+
+        Mechanism mechanism = Mechanism.valueOf(configuration.getStringValue("differentialPrivacy.mechanism"));
+        switch (mechanism) {
+            case BINARY:
+                String value1 = configuration.getStringValue("differentialPrivacy.binary.value1");
+                String value2 = configuration.getStringValue("differentialPrivacy.binary.value2");
+                options.setBinaryValues(value1, value2);
+                break;
+            case CATEGORICAL:
+                String hierarchyName = configuration.getStringValue("differentialPrivacy.categorical.hierarchyName");
+                JsonNode hierarchyMapNode = configuration.getJsonNodeValue("differentialPrivacy.categorical.hierarchyMap");
+
+                Map<String, GeneralizationHierarchy> hierarchyMap = AnonymizationOptions.hierarchiesFromJSON(hierarchyMapNode);
+                GeneralizationHierarchy hierarchy = hierarchyMap.get(hierarchyName);
+                if (hierarchy == null) {
+                    String msg = "Unable to find the hierarchy with name: " + hierarchyName;
+                    logger.error(msg);
+                    throw new MisconfigurationException(msg);
+                }
+                options.setHierarchy(hierarchy);
+                break;
+            case LAPLACE_NATIVE:
+            case LAPLACE_BOUNDED:
+            case LAPLACE_TRUNCATED:
+                double lowerBound = configuration.getDoubleValue("differentialPrivacy.range.min");
+                double upperBound = configuration.getDoubleValue("differentialPrivacy.range.max");
+                options.setBounds(lowerBound, upperBound);
+                break;
+        }
+        this.mechanism = DPMechanismFactory.getMechanism(mechanism);
+        this.mechanism.setOptions(options);
+    }
+
+    public DifferentialPrivacyMaskingProvider(DefaultMaskingConfiguration configuration) {
+        this(new SecureRandom(), configuration);
+    }
+
+    @Override
+    public String mask(String value) {
+        return mechanism.randomise(value);
+    }
+}
---
+++ library/src/test/java/com/ibm/research/drl/dpt/providers/masking/DifferentialPrivacyMaskingProviderTest.java
@@ -0,0 +1,221 @@
+/*******************************************************************
+ *                                                                 *
+ * Copyright IBM Corp. 2021                                        *
+ *                                                                 *
+ *******************************************************************/
+package com.ibm.research.drl.dpt.providers.masking;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.ibm.research.drl.dpt.anonymization.differentialprivacy.Mechanism;
+import com.ibm.research.drl.dpt.configuration.DefaultMaskingConfiguration;
+import com.ibm.research.drl.dpt.util.JsonUtils;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.in;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
+import static org.hamcrest.Matchers.not;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class DifferentialPrivacyMaskingProviderTest {
+    @Test
+    public void testThrowExceptionOnNoSpecifiedMechanism() {
+        assertThrows(Exception.class, () -> {
+            DefaultMaskingConfiguration configuration = new DefaultMaskingConfiguration();
+            configuration.setValue("differentialPrivacy.mechanism", null);
+            DifferentialPrivacyMaskingProvider provider = new DifferentialPrivacyMaskingProvider(configuration);
+            String value = provider.mask("1234");
+
+            assertNotNull(value);
+            assertThat(value,not("1234"));
+        });
+    }
+
+    @Test
+    public void worksOnDefaultConfiguration() {
+        DefaultMaskingConfiguration configuration = new DefaultMaskingConfiguration();
+        DifferentialPrivacyMaskingProvider provider = new DifferentialPrivacyMaskingProvider(configuration);
+        String value = provider.mask("1234");
+
+        assertNotNull(value);
+        assertThat(value, not("1234"));
+    }
+
+    @Test
+    public void testThrowExceptionOnNoEpsilon() {
+        assertThrows(Exception.class, () -> {
+            DefaultMaskingConfiguration configuration = new DefaultMaskingConfiguration();
+            configuration.setValue("differentialPrivacy.parameter.epsilon", null);
+            DifferentialPrivacyMaskingProvider provider = new DifferentialPrivacyMaskingProvider(configuration);
+            String value = provider.mask("1234");
+
+            assertNotNull(value);
+            assertThat(value, not("1234"));
+        });
+    }
+
+    @Test
+    public void testThrowExceptionOnNegEpsilon() {
+        assertThrows(Exception.class, () -> {
+            DefaultMaskingConfiguration configuration = new DefaultMaskingConfiguration();
+            configuration.setValue("differentialPrivacy.mechanism", Mechanism.BINARY.name());
+            configuration.setValue("differentialPrivacy.binary.value1", "1");
+            configuration.setValue("differentialPrivacy.binary.value2", "2");
+            configuration.setValue("differentialPrivacy.parameter.epsilon", -1.0);
+            DifferentialPrivacyMaskingProvider provider = new DifferentialPrivacyMaskingProvider(configuration);
+            String value = provider.mask("1");
+
+            assertNotNull(value);
+            assertThat(value, is(in(new String[]{"1", "2"})));
+        });
+    }
+
+    @Test
+    public void worksForBinary() {
+        DefaultMaskingConfiguration configuration = new DefaultMaskingConfiguration();
+        configuration.setValue("differentialPrivacy.mechanism", Mechanism.BINARY.name());
+        configuration.setValue("differentialPrivacy.binary.value1", "1");
+        configuration.setValue("differentialPrivacy.binary.value2", "2");
+        configuration.setValue("differentialPrivacy.parameter.epsilon", 0.1);
+        DifferentialPrivacyMaskingProvider provider = new DifferentialPrivacyMaskingProvider(configuration);
+        String value = provider.mask("1");
+
+        assertNotNull(value);
+        assertThat(value, is(in(new String[]{"1", "2"})));
+    }
+
+    @Test
+    public void worksForCategorical() throws Exception {
+        DefaultMaskingConfiguration configuration = new DefaultMaskingConfiguration();
+        configuration.setValue("differentialPrivacy.mechanism", Mechanism.CATEGORICAL.name());
+        configuration.setValue("differentialPrivacy.parameter.epsilon", 0.1);
+        configuration.setValue("differentialPrivacy.categorical.hierarchyName", "colors");
+        
+        JsonNode hierarchyMap = JsonUtils.MAPPER.readTree("{" +
+                "\"colors\": [" +
+                "[\"Red\", \"*\"]," +
+                "[\"Green\", \"*\"]," +
+                "[\"Blue\", \"*\"]" +
+                "]" +
+                "}");
+        
+        configuration.setValue("differentialPrivacy.categorical.hierarchyMap", hierarchyMap);
+        
+        DifferentialPrivacyMaskingProvider provider = new DifferentialPrivacyMaskingProvider(configuration);
+        Set<String> terms = new HashSet<>(Arrays.asList("RED", "GREEN", "BLUE"));
+        
+        for(int i = 0; i < 100; i++) {
+            String value = provider.mask("Red");
+            assertNotNull(value);
+            assertThat(value, is(in(terms)));
+        }
+    }
+
+    @Test
+    public void worksForLaplace() {
+        DefaultMaskingConfiguration configuration = new DefaultMaskingConfiguration();
+        configuration.setValue("differentialPrivacy.mechanism", Mechanism.LAPLACE_NATIVE.name());
+        configuration.setValue("differentialPrivacy.parameter.epsilon", 0.1);
+        configuration.setValue("differentialPrivacy.range.min", 0);
+        configuration.setValue("differentialPrivacy.range.max", 1);
+        DifferentialPrivacyMaskingProvider provider = new DifferentialPrivacyMaskingProvider(configuration);
+        String valueString = provider.mask("0.5");
+        Double value = Double.parseDouble(valueString);
+
+        assertNotNull(value);
+        assertFalse(value.isNaN());
+    }
+
+    @Test
+    public void worksForLaplaceBounded() {
+        DefaultMaskingConfiguration configuration = new DefaultMaskingConfiguration();
+        configuration.setValue("differentialPrivacy.mechanism", Mechanism.LAPLACE_BOUNDED.name());
+        configuration.setValue("differentialPrivacy.parameter.epsilon", 0.1);
+        configuration.setValue("differentialPrivacy.range.min", 0);
+        configuration.setValue("differentialPrivacy.range.max", 1);
+        DifferentialPrivacyMaskingProvider provider = new DifferentialPrivacyMaskingProvider(configuration);
+        String valueString = provider.mask("0.5");
+        Double value = Double.parseDouble(valueString);
+
+        assertNotNull(value);
+        assertFalse(value.isNaN());
+        assertThat(value, lessThanOrEqualTo(1.0));
+        assertThat(value, greaterThanOrEqualTo(0.0));
+    }
+
+    @Test
+    public void worksForLaplaceTruncated() {
+        DefaultMaskingConfiguration configuration = new DefaultMaskingConfiguration();
+        configuration.setValue("differentialPrivacy.mechanism", Mechanism.LAPLACE_TRUNCATED.name());
+        configuration.setValue("differentialPrivacy.parameter.epsilon", 0.1);
+        configuration.setValue("differentialPrivacy.range.min", 0);
+        configuration.setValue("differentialPrivacy.range.max", 1);
+        DifferentialPrivacyMaskingProvider provider = new DifferentialPrivacyMaskingProvider(configuration);
+        String valueString = provider.mask("0.5");
+        Double value = Double.parseDouble(valueString);
+
+        assertNotNull(value);
+        assertFalse(value.isNaN());
+        assertThat(value, lessThanOrEqualTo(1.0));
+        assertThat(value, greaterThanOrEqualTo(0.0));
+    }
+
+    @Test
+    public void wrongOrderOfBoundsLaplace() {
+        assertThrows(Exception.class, () -> {
+            DefaultMaskingConfiguration configuration = new DefaultMaskingConfiguration();
+            configuration.setValue("differentialPrivacy.mechanism", Mechanism.LAPLACE_NATIVE.name());
+            configuration.setValue("differentialPrivacy.parameter.epsilon", 0.1);
+            configuration.setValue("differentialPrivacy.range.min", 100);
+            configuration.setValue("differentialPrivacy.range.max", 0);
+            DifferentialPrivacyMaskingProvider provider = new DifferentialPrivacyMaskingProvider(configuration);
+            String valueString = provider.mask("0.5");
+            Double value = Double.parseDouble(valueString);
+
+            assertNotNull(value);
+            assertFalse(value.isNaN());
+        });
+    }
+
+    @Test
+    public void nullHierarchyCategorical() {
+        assertThrows(Exception.class, () -> {
+            DefaultMaskingConfiguration configuration = new DefaultMaskingConfiguration();
+            configuration.setValue("differentialPrivacy.mechanism", Mechanism.CATEGORICAL.name());
+            configuration.setValue("differentialPrivacy.parameter.epsilon", 0.1);
+            configuration.setValue("differentialPrivacy.categorical.set", null);
+            DifferentialPrivacyMaskingProvider provider = new DifferentialPrivacyMaskingProvider(configuration);
+            String value = provider.mask("Red");
+
+            List<String> terms = Arrays.asList("RED", "GREEN", "BLUE");
+
+            assertNotNull(value);
+            assertThat(value, is(in(terms)));
+        });
+    }
+
+    @Test
+    public void undefinedBinaryValue() {
+        assertThrows(Exception.class, () -> {
+            DefaultMaskingConfiguration configuration = new DefaultMaskingConfiguration();
+            configuration.setValue("differentialPrivacy.mechanism", Mechanism.BINARY.name());
+            configuration.setValue("differentialPrivacy.binary.value1", "1");
+            configuration.setValue("differentialPrivacy.binary.value2", null);
+            configuration.setValue("differentialPrivacy.parameter.epsilon", 0.1);
+            DifferentialPrivacyMaskingProvider provider = new DifferentialPrivacyMaskingProvider(configuration);
+            String value = provider.mask("1");
+
+            assertNotNull(value);
+            assertThat(value, is(in(new String[]{"1", "2"})));
+        });
+    }
+}
