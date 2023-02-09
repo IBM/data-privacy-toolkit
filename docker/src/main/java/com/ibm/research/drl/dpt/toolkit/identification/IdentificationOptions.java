5acf8de9d4e41c36b2a48fd763037f3e45fef9b1
Stefano Braghin <527806+stefano81@users.noreply.github.com>
2023-02-09 11:14:42 +0100

    refactor toolkit configuration to include extended identification configuration
    Signed-off-by: Stefano Braghin <527806+stefano81@users.noreply.github.com>

--- docker/src/main/java/com/ibm/research/drl/dpt/toolkit/identification/IdentificationOptions.java
+++ docker/src/main/java/com/ibm/research/drl/dpt/toolkit/identification/IdentificationOptions.java
@@ -8,30 +8,36 @@ package com.ibm.research.drl.dpt.toolkit.identification;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.ibm.research.drl.dpt.configuration.IdentificationConfiguration;
 import com.ibm.research.drl.dpt.toolkit.task.TaskOptions;
 
 import java.util.List;
+import java.util.Objects;
 
 public class IdentificationOptions extends TaskOptions {
     private String localization;
     private int firstN;
-    private final List<JsonNode> identifiers;
+    private final JsonNode identifiers;
     private ComplianceFramework framework;
     private boolean buildMaskingConfiguration;
+
+    private final IdentificationConfiguration configuration;
 
     @JsonCreator
     public IdentificationOptions(
             @JsonProperty("localization") String localization,
             @JsonProperty("firstN") int firstN,
-            @JsonProperty("identifiers") List<JsonNode> identifiers,
+            @JsonProperty("identifiers") JsonNode identifiers,
             @JsonProperty("complianceFramework") ComplianceFramework framework,
-            @JsonProperty("buildMaskingConfiguration") boolean buildMaskingConfiguration
+            @JsonProperty("buildMaskingConfiguration") boolean buildMaskingConfiguration,
+            @JsonProperty("configuration") IdentificationConfiguration configuration
     ) {
         this.localization = localization;
         this.firstN = firstN;
         this.identifiers = identifiers;
         this.framework = framework;
         this.buildMaskingConfiguration = buildMaskingConfiguration;
+        this.configuration = Objects.requireNonNullElse(configuration, IdentificationConfiguration.DEFAULT);
     }
 
     public String getLocalization() {
@@ -42,7 +48,7 @@ public class IdentificationOptions extends TaskOptions {
         return firstN;
     }
 
-    public List<JsonNode> getIdentifiers() {
+    public JsonNode getIdentifiers() {
         return identifiers;
     }
 
@@ -68,5 +74,9 @@ public class IdentificationOptions extends TaskOptions {
 
     public boolean getBuildMaskingConfiguration() {
         return buildMaskingConfiguration;
+    }
+
+    public IdentificationConfiguration getConfiguration() {
+        return configuration;
     }
 }
