844737abb7bdf549dcb9326a30091e29128ad22f
Stefano Braghin <527806+stefano81@users.noreply.github.com>
2023-02-17 10:38:29 +0000

    add German TIN
    Signed-off-by: Stefano Braghin <527806+stefano81@users.noreply.github.com>

---
+++ library/src/test/java/com/ibm/research/drl/dpt/providers/identifiers/GermanTaxIdentificationNumberIdentifierTest.java
@@ -0,0 +1,41 @@
+package com.ibm.research.drl.dpt.providers.identifiers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+
+class GermanTaxIdentificationNumberIdentifierTest {
+    @Test
+    public void testPositive() {
+        Identifier identifier = new GermanTaxIdentificationNumberIdentifier();
+
+        String[] correctValues = new String[] {
+                "96480255173",
+                "96480255173",
+                "96580255173",
+                "96580255173",
+        };
+
+        for (String value : correctValues) {
+            assertThat(value, identifier.isOfThisType(value), is(true));
+        }
+    }
+
+    @Test
+    public void testNegative() {
+        Identifier identifier = new GermanTaxIdentificationNumberIdentifier();
+
+        String[] invalidValues = new String[] {
+                "06480255173",
+                "96480255171",
+                "96480255573",
+                "96580245173",
+        };
+
+        for (String value : invalidValues) {
+            assertThat(value, identifier.isOfThisType(value), is(false));
+        }
+    }
+}
