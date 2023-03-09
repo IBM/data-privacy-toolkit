48505d9fa62e94e94dddf7a2f338b41266161c05
Stefano Braghin <527806+stefano81@users.noreply.github.com>
2023-03-09 11:33:15 +0000

    cleanup
    Signed-off-by: Stefano Braghin <527806+stefano81@users.noreply.github.com>

--- library/src/main/java/com/ibm/research/drl/dpt/schema/RelationshipType.java
+++ library/src/main/java/com/ibm/research/drl/dpt/schema/RelationshipType.java
@@ -6,40 +6,15 @@
 package com.ibm.research.drl.dpt.schema;
 
 public enum RelationshipType {
-    /**
-     * Sum relationship type.
-     */
     SUM,
-    /**
-     * Sum approximate relationship type.
-     */
     SUM_APPROXIMATE,
-    /**
-     * Product relationship type.
-     */
     PRODUCT,
-    /**
-     * Equals relationship type.
-     */
     EQUALS,
-    /**
-     * Greater relationship type.
-     */
     GREATER,
-    /* For dates */
     DISTANCE,
-    /**
-     * Less relationship type.
-     */
     LESS,
-    /**
-     * Linked relationship type.
-     */
     LINKED,
-
     KEY,
-
     RATIO,
-
     GREP_AND_MASK /* for freetext */
 }
