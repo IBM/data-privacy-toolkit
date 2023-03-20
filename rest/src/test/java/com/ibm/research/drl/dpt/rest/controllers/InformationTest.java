6d45fd751b3693b3ced7aa6ed85d664c8bcf89ed
Stefano Braghin <527806+stefano81@users.noreply.github.com>
2023-03-20 13:00:41 +0000

    fix temporarily identification to not accept sampling, jackson conflicting version with SpringBoot
    Signed-off-by: Stefano Braghin <527806+stefano81@users.noreply.github.com>

--- rest/src/test/java/com/ibm/research/drl/dpt/rest/controllers/InformationTest.java
+++ rest/src/test/java/com/ibm/research/drl/dpt/rest/controllers/InformationTest.java
@@ -1,3 +1,21 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+*/
 package com.ibm.research.drl.dpt.rest.controllers;
 
 import org.junit.jupiter.api.Test;
@@ -25,7 +43,7 @@ class InformationTest {
         mvc.perform(get("/api/information/identifiers").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$", hasSize(26)));
+                .andExpect(jsonPath("$", hasSize(33)));
     }
 
     @Test
